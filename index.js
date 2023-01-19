3d9f7f3c0ca5eec24d5916c60d7616d01b7b74d7
Emmanuel Saura <emmanuelsaura007@gmail.com>
2023-01-19 18:38:06 +0300

    my commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let count = 0;
+let countEL = document.getElementById("results");
 function add() {
   count++;
+
   document.getElementById("count").innerHTML = count;
 }
 function sub() {
   count--;
   document.getElementById("count").innerHTML = count;
+}
+function save() {
+  let countstr = count + "_";
+  countEL.innerText += countstr;
 }
